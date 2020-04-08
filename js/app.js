50f7965497deef218086624b731b438ca5d3c793
LuniaMage <luniamage@gmail.com>
2020-04-08 13:05:10 -0400

    second commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// box1 = doument.querySelector('box-1').onclick = function moveExampe() {
+    
+// };
+// box2 = doument.querySelector('box-2');
+// box3 = doument.querySelector('box-3');
+// box4 = doument.querySelector('box-4');
+
+
+
